3d5a81c6606b847b75801fc75b91c4b621dfac8c
Franz Enzenhofer <f.enzenhofer@tupalo.com>
2011-06-06 16:19:32 +0200

    progress

--- server.coffee
+++ server.coffee
@@ -1,11 +1,13 @@
 express = require 'express'
 util = require 'util'
 parseUri = require 'parseUri'
+#robotsTxt = require '../robotstxt/index.js'
 robotsTxt = require 'robotstxt'
 
 
 robotstxturi = 'http://www.example.com/robots.txt'
 totestA = []
+useragent = ''
 
 
 app = express.createServer()
@@ -13,73 +15,94 @@ app.use express.static __dirname + '/public'
 app.set 'views', __dirname + '/views'
 app.set 'view engine', 'jade'
 
+
+indexRender = (res, title='Robots.Txt Checker', des='Crawl, parse and test Robots.txt Files', msg, robotstxturi, totestA, useragent) ->
+
+  res.render 'index.jade', {
+    title: title
+    description: des
+    msg:msg
+    robotstxturi: robotstxturi
+    totestA: totestA
+    useragent:useragent
+  }
+
+
+
 #listen on /
 app.get '/', (req, res) ->
   msg = 
     error: []
-    note: []
+    notes: []
     results: []
   
-  console.log req.query
+  #console.log req.query
   
   #parse robots.txt and find out if it is a valid robots.txt uri
   rt = parseUri req.query.robotstxturl if req.query?.robotstxturl? 
   if rt?.path? and rt.path isnt ''
-    console.log rt
+    #console.log rt
     rtl = rt.path.toLowerCase().trim()
     
     #its not a valid robots.txt uri
     if rtl isnt '/robots.txt'
-      console.log 'not a valid robots.txt url'
+      #console.log 'not a valid robots.txt url'
       msg.error.push 'given robots.txt url is not a valid robots.txt url (must end in /robots.txt)'
     
     #its a valid robots.txt uri
     else 
       #trim the robots.txt uri string
       robotstxturi = req.query.robotstxturl.trim()
+      useragent = req.query.useragent.trim()
       
       #prepare the test uris
       if req.query.testurls?
         totestA = req.query.testurls.split "\n"
-        preParseTestUrl = (x) -> 
-          x = x.trim()
-          if x[0]? and x[0].toLowerCase() isnt 'h' and x[0] isnt '/'
-            x = ['/', x].join ''
-          xu = parseUri(x)
-          if xu.path? and xu.path isnt ''
-            xu.path
-          else
-            null
-        totestA = (preParseTestUrl x for x in totestA)
-        tempA = []
-        (x) -> if x then tempA.push x for x in totestA  
-        totestA = tempA
-        console.log 'totestA'
-        console.log totestA
+        
+        #get rid of all unvalid values (i.e.:blanks)
+        preParseTestUrls = (xA) ->
+          preParseTestUrl = (x) -> 
+            x = x.trim()
+            if x[0]? and x[0].toLowerCase() isnt 'h' and x[0] isnt '/'
+              x = ['/', x].join ''
+            xu = parseUri(x)
+            if xu.path? and xu.path isnt ''
+              xu.path
+            else
+              null
+          
+          xA = (preParseTestUrl x for x in xA)
+          tempA = []
+          (tempA.push(x) if x?) for x in xA
+          return tempA
+        
+        totestA = preParseTestUrls totestA 
+        #console.log 'totestA'
+        #console.log totestA
       else
         msg.error.push 'no test uris given'
       
       #create a new gate keeper
-      rt = robotsTxt robotstxturi
+      rt = robotsTxt robotstxturi, useragent
       
       #when the gate keeper is ready
       rt.on 'ready', (gate_keeper) ->
-        console.log gate_keeper
-        testdata = (gate_keeper.why y for y in totestA)
-        console.log testdata
-        console.log testdata[0].rules
+        #console.log gate_keeper
+        msg.results = (gate_keeper.why y for y in totestA)
+        msg.notes.push "#{msg.results.length} URLs successfully tested"
+        ##console.log msg.results
+        console.log msg.results[0].rules
+        ##console.log msg
+        console.log 'RENDER WITH DATA'
+        ##console.log msg
+        indexRender(res, 'Robots.Txt Checker', 'a description', msg, robotstxturi, totestA, useragent)
   else
-    msg.note.push 'please ente a valid robots.txt url and some test urls'
+    msg.notes.push 'please ente a valid robots.txt url and some test urls'
+    console.log '##########RENDER WITHOUT DATA'
+    indexRender(res, 'Robots.Txt Checker', 'a description', msg, robotstxturi, totestA)
   
-  console.log msg
+  ##console.log msg
     
+
   
-  
-  res.render 'index.jade', {
-  title: 'Robots.Txt Checker'
-  description:null
-  msg:msg
-  robotstxturi: robotstxturi
-  totestA: totestA
-  }
 app.listen(3003);
--- server.js
+++ server.js
@@ -1,62 +1,108 @@
 (function() {
-  var app, express, parseUri, robotsTxt, util;
+  var app, express, indexRender, parseUri, robotsTxt, robotstxturi, totestA, util;
   express = require('express');
   util = require('util');
   parseUri = require('parseUri');
   robotsTxt = require('robotstxt');
+  robotstxturi = 'http://www.example.com/robots.txt';
+  totestA = [];
   app = express.createServer();
   app.use(express.static(__dirname + '/public'));
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  indexRender = function(res, title, des, msg, robotstxturi, totestA) {
+    if (title == null) {
+      title = 'Robots.Txt Checker';
+    }
+    if (des == null) {
+      des = 'Crawl, parse and test Robots.txt Files';
+    }
+    return res.render('index.jade', {
+      title: title,
+      description: des,
+      msg: msg,
+      robotstxturi: robotstxturi,
+      totestA: totestA
+    });
+  };
   app.get('/', function(req, res) {
-    var msg, rt, rtl, totestA, _ref;
+    var msg, preParseTestUrls, rt, rtl, _ref;
     msg = {
       error: [],
-      note: [],
+      notes: [],
       results: []
     };
-    console.log(req.query);
     if (((_ref = req.query) != null ? _ref.robotstxturl : void 0) != null) {
       rt = parseUri(req.query.robotstxturl);
     }
     if (((rt != null ? rt.path : void 0) != null) && rt.path !== '') {
-      console.log(rt);
       rtl = rt.path.toLowerCase().trim();
       if (rtl !== '/robots.txt') {
-        console.log('not a valid robots.txt url');
-        msg.error.push('given robots.txt url is not a valid robots.txt url (must end in /robots.txt)');
+        return msg.error.push('given robots.txt url is not a valid robots.txt url (must end in /robots.txt)');
       } else {
-        req.query.robotstxturl = req.query.robotstxturl.trim();
+        robotstxturi = req.query.robotstxturl.trim();
         if (req.query.testurls != null) {
           totestA = req.query.testurls.split("\n");
-          totestA = (function(x) {
-            var _i, _len, _results;
-            _results = [];
-            for (_i = 0, _len = totestA.length; _i < _len; _i++) {
-              x = totestA[_i];
-              _results.push(x.trim());
+          preParseTestUrls = function(xA) {
+            var preParseTestUrl, tempA, x, _i, _len;
+            preParseTestUrl = function(x) {
+              var xu;
+              x = x.trim();
+              if ((x[0] != null) && x[0].toLowerCase() !== 'h' && x[0] !== '/') {
+                x = ['/', x].join('');
+              }
+              xu = parseUri(x);
+              if ((xu.path != null) && xu.path !== '') {
+                return xu.path;
+              } else {
+                return null;
+              }
+            };
+            xA = (function() {
+              var _i, _len, _results;
+              _results = [];
+              for (_i = 0, _len = xA.length; _i < _len; _i++) {
+                x = xA[_i];
+                _results.push(preParseTestUrl(x));
+              }
+              return _results;
+            })();
+            tempA = [];
+            for (_i = 0, _len = xA.length; _i < _len; _i++) {
+              x = xA[_i];
+              if (x != null) {
+                tempA.push(x);
+              }
             }
-            return _results;
-          });
-          console.log('totestA');
-          console.log(totestA);
+            return tempA;
+          };
+          totestA = preParseTestUrls(totestA);
         } else {
           msg.error.push('no test uris given');
         }
-        rt = robotsTxt(req.query.robotstxturl);
-        rt.on('ready', function(gate_keeper) {
-          return console.log(gate_keeper);
+        rt = robotsTxt(robotstxturi);
+        return rt.on('ready', function(gate_keeper) {
+          var y;
+          msg.results = (function() {
+            var _i, _len, _results;
+            _results = [];
+            for (_i = 0, _len = totestA.length; _i < _len; _i++) {
+              y = totestA[_i];
+              _results.push(gate_keeper.why(y));
+            }
+            return _results;
+          })();
+          msg.notes.push("" + msg.results.length + " URLs successfully tested");
+          console.log('RENDER WITH DATA');
+          console.log(msg);
+          return indexRender(res, 'Robots.Txt Checker', 'a description', msg, robotstxturi, totestA);
         });
       }
     } else {
-      msg.note.push('please ente a valid robots.txt url and some test urls');
+      msg.notes.push('please ente a valid robots.txt url and some test urls');
+      console.log('##########RENDER WITHOUT DATA');
+      return indexRender(res, 'Robots.Txt Checker', 'a description', msg, robotstxturi, totestA);
     }
-    console.log(msg);
-    return res.render('index.jade', {
-      title: 'Robots.Txt Checker',
-      description: null,
-      msg: msg
-    });
   });
   app.listen(3003);
 }).call(this);
